f9fbc814cb543ffe0ee4de803fd9df20db572004
kkkk <kkkk@KKKK-PC>
2019-06-24 20:39:05 +0800

    version 2 nn (single hidden layer)

--- practice2_iostream/practice2_iostream/practice2_iostream.cpp
+++ practice2_iostream/practice2_iostream/practice2_iostream.cpp
@@ -2,8 +2,81 @@
 #include <iostream>
 #include <string>
 
+class Vector {
+public:
+	//the ':' here means overwrite value in initial
+	Vector(int s) :_elem{ new double[s] }, _sz{ s }
+	{
+	}
+
+	//overload [] operator, so we can use v[i] to take array value
+	//return double reference
+	double&  operator[](int i)
+	{ 
+		return _elem[i];
+	}
+	int size() 
+	{
+		return _sz;
+	}
+
+private:
+	double *_elem;
+	int _sz;
+};
+
+enum class Color{red, blue, green};
+enum class TrafficLight { yellow, red, green };
+
+/*
+template <typename T>
+T minimum(const T &lhs, const T & rhs) {
+	return (lhs < rhs) ? lhs : rhs;
+}
+
+int get_a(void) {
+	return 5;
+}
+
+int get_b(void) {
+	return 4;
+}
+*/
+
+double read_and_sum(int s)
+{
+	Vector v(s); // make a vector of s elements
+	for (int i = 0; i < v.size(); ++i) std::cin >> v[i]; // read into elements
+	
+	double sum = 0;
+	for (int i = 0; i < v.size(); ++i) sum += v[i]; // take the sum of the elements
+	return sum;
+}
+TrafficLight& operator++(TrafficLight& t) // prefix increment: ++
+{
+	switch(t){
+		case TrafficLight::green: return t = TrafficLight::yellow;
+		case TrafficLight::yellow: return t = TrafficLight::red;
+		case TrafficLight::red: return t = TrafficLight::green;
+	}
+}
 int main()
 {
+	Color col = Color::green;
+	TrafficLight light = TrafficLight::yellow;
+	TrafficLight next = ++light;
+	std::cout << light << "," << next << std::endl;
+
+	std::cout << read_and_sum(5);
+
+	/*
+	int x = get_a();
+	int b = get_b();
+	int i = minimum(x, b);
+	std::cout << i << std::endl;
+	std::cout << &b << "," << &i << std::endl;
+	*/
+
 	//std::cout.put('SS');
 	//std::cout << "\n" << 3.0;
 
